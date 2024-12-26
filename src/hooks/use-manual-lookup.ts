77a4b7daea6a55e6fbc4fb37732718d6da4aaca0
thucpn <thucsh2@gmail.com>
2024-12-26 12:07:46 +0700

    get current domain

--- src/hooks/use-manual-lookup.ts
+++ src/hooks/use-manual-lookup.ts
@@ -8,10 +8,9 @@ export function useManualLookup() {
   useEffect(() => {
     async function fetchManual() {
       try {
-        const url = new URL(window.location.href);
-        const hostname = url.hostname; // eg. linkedin.com
+        const rootHostname = getCurrentDomain();
         const response = await fetch(
-          `${backend}/manuals?url=${encodeURIComponent(hostname)}`,
+          `${backend}/manuals?url=${encodeURIComponent(rootHostname)}`,
         );
         const data = (await response.json()) as { content: string };
         setManual(data.content);
@@ -25,4 +24,17 @@ export function useManualLookup() {
   }, []);
 
   return manual;
+}
+
+/**
+ * Extracts the root domain from the current URL.
+ * Eg: current URL is https://www.linkedin.com/feed/ -> returns linkedin.com
+ * @returns The root domain as a string.
+ */
+function getCurrentDomain() {
+  const parsedUrl = new URL(window.location.href);
+  const hostname = parsedUrl.hostname;
+  const parts = hostname.split(".");
+  const rootHostname = parts.slice(-2).join(".");
+  return rootHostname;
 }
