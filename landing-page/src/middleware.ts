c8f1421a0cea600f12970eb18a8bf8e667a5622f
thucpn <thucsh2@gmail.com>
2024-12-27 09:40:43 +0700

    merge middleware

--- landing-page/src/middleware.ts
+++ landing-page/src/middleware.ts
@@ -1,5 +1,52 @@
-import { type NextRequest } from "next/server";
-import { updateSession } from "@/utils/supabase/middleware";
+import { createServerClient } from "@supabase/ssr";
+import { type NextRequest, NextResponse } from "next/server";
+
+const updateSession = async (request: NextRequest) => {
+  let response = NextResponse.next({
+    request: {
+      headers: request.headers,
+    },
+  });
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return request.cookies.getAll();
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value }) =>
+            request.cookies.set(name, value),
+          );
+          response = NextResponse.next({
+            request,
+          });
+          cookiesToSet.forEach(({ name, value, options }) =>
+            response.cookies.set(name, value, options),
+          );
+        },
+      },
+    },
+  );
+
+  // This will refresh session if expired - required for Server Components
+  // https://supabase.com/docs/guides/auth/server-side/nextjs
+  const user = await supabase.auth.getUser();
+
+  // protected routes
+  if (request.nextUrl.pathname.startsWith("/dashboard") && user.error) {
+    return NextResponse.redirect(new URL("/auth", request.url));
+  }
+
+  // redirect to dashboard if user is already logged in and trying to access auth page
+  if (request.nextUrl.pathname === "/auth" && !user.error) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  return response;
+};
 
 export async function middleware(request: NextRequest) {
   return await updateSession(request);
