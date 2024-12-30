803424d8290600bbada99a673cd63de4fa2e470d
thucpn <thucsh2@gmail.com>
2024-12-30 11:15:00 +0700

    fix: lint

--- backend/src/app/auth/page.tsx
+++ backend/src/app/auth/page.tsx
@@ -1,11 +1,11 @@
 import { AuthForm } from "@/components/auth/AuthForm";
 
 export default function AuthPage() {
-	return (
-		<div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
-			<div className="flex w-full max-w-sm flex-col gap-6">
-				<AuthForm />
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <AuthForm />
+      </div>
+    </div>
+  );
 }
