9b3c7085c3b7d32efdf1a2c6f88eaa5050bd1901
thucpn <thucsh2@gmail.com>
2024-12-31 15:06:33 +0700

    change to admin page

--- backend/src/components/AppSidebar.tsx
+++ backend/src/components/AppSidebar.tsx
@@ -2,74 +2,67 @@
 
 import { NavUser } from "@/components/NavUser";
 import {
-  Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarGroup,
-  SidebarGroupLabel,
-  SidebarHeader,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
-  SidebarRail,
-  useSidebar,
+	Sidebar,
+	SidebarContent,
+	SidebarFooter,
+	SidebarGroup,
+	SidebarGroupLabel,
+	SidebarHeader,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
+	SidebarRail,
+	useSidebar,
 } from "@/components/ui/sidebar";
-import { BadgeCheck, Headset, Settings2 } from "lucide-react";
-import * as React from "react";
-import Logo from "./Logo";
+import { Headset, Settings2 } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import * as React from "react";
+import Logo from "./Logo";
 
 const sidebarItems = [
-  {
-    name: "Account",
-    url: "/account",
-    icon: BadgeCheck,
-  },
-  {
-    name: "Settings",
-    url: "/account/settings",
-    icon: Settings2,
-  },
+	{
+		name: "Settings",
+		url: "/admin/settings",
+		icon: Settings2,
+	},
 ];
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
-  const pathname = usePathname();
-  return (
-    <Sidebar collapsible="icon" {...props}>
-      <AppSidebarHeader />
-      <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupLabel>Platform</SidebarGroupLabel>
-          <SidebarMenu>
-            {sidebarItems.map((item) => (
-              <SidebarMenuItem key={item.name}>
-                <SidebarMenuButton asChild isActive={pathname === item.url}>
-                  <Link href={item.url}>
-                    <item.icon />
-                    <span>{item.name}</span>
-                  </Link>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-            ))}
-          </SidebarMenu>
-        </SidebarGroup>
-      </SidebarContent>
-      <SidebarFooter>
-        <NavUser />
-      </SidebarFooter>
-      <SidebarRail />
-    </Sidebar>
-  );
+	const pathname = usePathname();
+	return (
+		<Sidebar collapsible="icon" {...props}>
+			<AppSidebarHeader />
+			<SidebarContent>
+				<SidebarGroup>
+					<SidebarGroupLabel>Platform</SidebarGroupLabel>
+					<SidebarMenu>
+						{sidebarItems.map((item) => (
+							<SidebarMenuItem key={item.name}>
+								<SidebarMenuButton asChild isActive={pathname === item.url}>
+									<Link href={item.url}>
+										<item.icon />
+										<span>{item.name}</span>
+									</Link>
+								</SidebarMenuButton>
+							</SidebarMenuItem>
+						))}
+					</SidebarMenu>
+				</SidebarGroup>
+			</SidebarContent>
+			<SidebarFooter>
+				<NavUser />
+			</SidebarFooter>
+			<SidebarRail />
+		</Sidebar>
+	);
 }
 
 function AppSidebarHeader() {
-  const { open } = useSidebar();
-  return (
-    <SidebarHeader>
-      <Link href="/">
-        {open ? <Logo /> : <Headset className="text-blue-900 mx-auto" />}
-      </Link>
-    </SidebarHeader>
-  );
+	const { open } = useSidebar();
+	return (
+		<SidebarHeader>
+			<Link href="/">{open ? <Logo /> : <Headset className="text-blue-900 mx-auto" />}</Link>
+		</SidebarHeader>
+	);
 }
