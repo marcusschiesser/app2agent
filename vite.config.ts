149f076d2444a2b1814733a1b2e865d678dd8077
thucpn <thucsh2@gmail.com>
2024-12-31 15:50:43 +0700

    test: husky

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,10 @@ if (!variant) {
   throw new Error("VARIANT environment variable is not defined");
 }
 
+if (!process.env.GEMINI_API_KEY) {
+  throw new Error("GEMINI_API_KEY environment variable is not defined");
+}
+
 const outDir = `dist/${variant}`;
 
 function generateManifest() {
