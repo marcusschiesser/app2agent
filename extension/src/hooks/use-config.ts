c406b5dbca25a5d512692009be6b4b1a17e1ce9d
leehuwuj <leehuwuj@gmail.com>
2025-01-03 09:38:59 +0700

    remove unnecessary change

--- extension/src/hooks/use-config.ts
+++ extension/src/hooks/use-config.ts
@@ -3,7 +3,7 @@ import { secureFetch } from "@/lib/secure-fetch";
 
 const backend =
   process.env.NODE_ENV === "development"
-    ? "https://www.app2agent.com/api"
+    ? "http://localhost:3000/api"
     : "https://www.app2agent.com/api";
 
 export type UserConfig = {
