147e3ab6818b6bee867dd13737e4990b15255b09
Marcus Schiesser <mail@marcusschiesser.de>
2025-01-13 17:59:28 +0700

    remove tab streaming

--- extension/src/background/service-worker.ts
+++ extension/src/background/service-worker.ts
@@ -1,11 +1,7 @@
-import { handleTabCapture, handleTabScreenshot } from "./recorder-controller";
+import { handleTabScreenshot } from "./recorder-controller";
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   switch (message.type) {
-    case "GET_TAB_STREAM":
-      handleTabCapture(sender, sendResponse);
-      return true;
-
     case "TAKE_SCREENSHOT":
       handleTabScreenshot(sender, sendResponse);
       return true;
