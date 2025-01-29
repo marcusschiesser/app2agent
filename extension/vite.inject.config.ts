50b2083343827658838508f72028d0006839dba6
Marcus Schiesser <mail@marcusschiesser.de>
2025-01-29 17:21:07 +0700

    add dev server

--- extension/vite.inject.config.ts
+++ extension/vite.inject.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "node:path";
 
+console.log("NODE_ENV:", process.env.NODE_ENV);
+
 export default defineConfig({
   build: {
     outDir: "dist/inject",
