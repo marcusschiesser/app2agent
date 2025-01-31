fadb14aa3ac8fd74221f79a37eecd97ff702c02e
Marcus Schiesser <mail@marcusschiesser.de>
2025-01-31 14:52:10 +0700

    fix base url

--- backend/src/lib/url.ts
+++ backend/src/lib/url.ts
@@ -1,7 +1,7 @@
 export function getBaseUrl() {
-  return (
+  const baseUrl =
     process.env.NEXT_PUBLIC_SITE_URL ??
     process.env.NEXT_PUBLIC_VERCEL_URL ??
-    "http://localhost:3000"
-  );
+    "http://localhost:3000";
+  return baseUrl.startsWith("http") ? baseUrl : `https://${baseUrl}`;
 }
